3a6bc25623478f114d45f1590dbbf2014e1686c2
Leo <leo@quotespire.com>
2013-01-20 09:45:28 -0800

    new update

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,9 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
-  validates :bio, presence: true
-  validates :twitter, presence: true
+  validates :bio, presence: false
+  validates :twitter, presence: false
+  validates :routine, length: { maximum: 5000 }
 
   private
 
